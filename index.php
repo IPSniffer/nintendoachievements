be76717b9a8f1244ec091ac17fbc7388457a66a4
IPSniffer <kishoyr@yahoo.com>
2020-10-05 10:13:08 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<title><?php echo Welcome?></title>
+
+<?php
+echo'
+<body style= "background-color:#2b2a2a;">
+'
+?>
+
+<?php
+echo ' <a href="index.php"> <img src= "TestBanner4_cr.png" title= "Home" /></a>
+';
+?>
+
+
+
+<?php
+echo' <span style="font-weight: bold; color: red; font-size: 30px";  <b><center> Welcome to the Nintendo Achievement Manager</center></b><br>
+'?>
+
+<?php
+echo'
+<div>
+	<center><label>Username:</label><br> <input type="text" id="username" name="username">
+</div>
+
+<div>
+	<center><label>Password:</label><br> <input type="password" id="password" name="password">
+</div>
+<center><input type="submit" value="Sign In"> '
+?>
+
+
+</body>
+</html>
+
