e11d7b52fb9b498b031197a2e870d03f7425a2e3
IPSniffer <kishoyr@yahoo.com>
2020-10-19 20:04:35 +0100

    Changes made

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
+
+
+
 <html>
 <body>
 
 <title><?php echo Welcome?></title>
 
+
 <?php
 echo'
-<body style= "background-color:#2b2a2a;">
+<body style= "background-color:#2b2a2a;"
 '
 ?>
 
@@ -21,19 +25,15 @@ echo ' <a href="index.php"> <img src= "TestBanner4_cr.png" title= "Home" /></a>
 echo' <span style="font-weight: bold; color: red; font-size: 30px";  <b><center> Welcome to the Nintendo Achievement Manager</center></b><br>
 '?>
 
-<?php
-echo'
-<div>
-	<center><label>Username:</label><br> <input type="text" id="username" name="username">
-</div>
+<form action="index.php" method="POST">
+	Email:
+	<input type="text" id="usr_email" name="usr_email" />
+	Password
+	<input type="<center>password" id="usr_password" name="usr_password" />
+	<input name= "submitForm" id="submitForm" type="submit" value="Login" />
 
-<div>
-	<center><label>Password:</label><br> <input type="password" id="password" name="password">
-</div>
-<center><input type="submit" value="Sign In"> '
-?>
+
 
 
 </body>
 </html>
-
