36e1cf3f69c52d42a7645e04b73e1e1dbc5171fa
IPSniffer <kishoyr@yahoo.com>
2020-11-09 09:22:59 +0000

    Finale changes soon

--- includes/banner.php
+++ includes/banner.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta name="banner" content="width=device-width, initial-scale=1">
-<a href="home.php"> <img src= "img/TestBanner5_cr.png" style="width: 100%" title= "Home" /></a>
+<center><a href="home.php"> <img src= "img/NintendoAchievements.png" title= "Home" /></a>
 
 
 
