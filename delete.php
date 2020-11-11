3128cade33d199fcbe5729a4df44e3dd04f30739
IPSniffer <72193108+IPSniffer@users.noreply.github.com>
2020-11-11 15:35:15 +0000

    Changes made

--- delete.php
+++ delete.php
@@ -2,28 +2,28 @@
 include 'includes/functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
-// Check that the poll ID exists
+
 if (isset($_GET['id'])) {
-    // Select the record that is going to be deleted
+
     $stmt = $pdo->prepare('SELECT * FROM polls WHERE id = ?');
     $stmt->execute([$_GET['id']]);
     $poll = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$poll) {
         die ('Poll doesn\'t exist with that ID!');
     }
-    // Make sure the user confirms beore deletion
+
     if (isset($_GET['confirm'])) {
         if ($_GET['confirm'] == 'yes') {
-            // User clicked the "Yes" button, delete record
+
             $stmt = $pdo->prepare('DELETE FROM polls WHERE id = ?');
             $stmt->execute([$_GET['id']]);
-            // We also need to delete the answers for that poll
+
             $stmt = $pdo->prepare('DELETE FROM poll_answers WHERE poll_id = ?');
             $stmt->execute([$_GET['id']]);
-            // Output msg
+
             $msg = 'You have deleted the poll!';
         } else {
-            // User clicked the "No" button, redirect them back to the home/index page
+
             header('Location: poll.php');
             exit;
         }
